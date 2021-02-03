6a717670170ce6138f0b05a8a5d0c5e17b3c5b72
Rosa-Eau <tory020900@gmail.com>
2021-02-03 09:30:38 +0900

    README 파일 수정

---
+++ java004/src/com/callor/hello/Hello.java
@@ -0,0 +1,20 @@
+package com.callor.hello;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		
+		int num1 = 22;
+		int num2 =33;
+		
+		System.out.println(num1 + num2);
+		System.out.println(num1 * num2);
+		System.out.println(num2 - num1);
+		System.out.println(num2 / num1);
+		System.out.println(num2 % num1);
+
+		
+		
+	}
+
+}
