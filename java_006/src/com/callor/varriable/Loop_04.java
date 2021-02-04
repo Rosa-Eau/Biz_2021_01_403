5226e9f01b8d4edd30fda395e6d7669b14d08f82
Rosa-Eau <tory0200900@gmail.com>
2021-02-04 16:12:34 +0900

    for문

--- java_006/src/com/callor/varriable/Loop_04.java
+++ java_006/src/com/callor/varriable/Loop_04.java
@@ -27,8 +27,24 @@ public class Loop_04 {
 			num1 += 1;
 		}
 		System.out.println("==================================");
+		/*
+		 * for() 명령문이 실행되는 순서
+		 * 최초에 한번 (1) 실행해서 num1을 1로 세팅
+		 * (2) 실행해서 num1 < 10 물어보기
+		 * 결과가 '예'이면
+		 * 		(3)~(7) 순서대로 실행
+		 * (8) 실행해서 num1 을 1증가
+		 * 다시 (2)를 실행해서 num1 < 10 물어보기
+		 * 이렇게 반복
+		 * num1<10이 아니오가 될 때까지
+		 */
+		//		(1)		(2)		(8)	
 		for(num1 =1; num1 < 10; num1 +=1) {
-			System.out.println(num1);
+			System.out.print(intDan); //(3)
+			System.out.print(" x "); //(4)
+			System.out.print(num1); //(5)
+			System.out.print(" = "); //(6)
+			System.out.println(intDan * num1); //(7)
 		}
 		
 	}
